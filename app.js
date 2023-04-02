7497cacad76c136fd26f3fa40277eb917920756b
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2023-04-02 14:42:51 +0530

    Chain Route Handlers using Express

--- app.js
+++ app.js
@@ -16,29 +16,27 @@ const articleSchema = new mongoose.Schema({ title: String, content: String });
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function (req, res) {
-    Article.find()
-        .then((articles) => res.send(articles))
-        .catch((error) => res.send(error));
-});
-
-app.post("/articles", function (req, res) {
-    const article = new Article({
-        title: req.body.title,
-        content: req.body.content,
+app.route("/articles")
+    .get(function (_, res) {
+        Article.find()
+            .then((articles) => res.send(articles))
+            .catch((error) => res.send(error));
+    })
+    .post(function (req, res) {
+        const article = new Article({
+            title: req.body.title,
+            content: req.body.content,
+        });
+        article
+            .save()
+            .then(() => res.send("Successfully added a new article"))
+            .catch((error) => res.send(error));
+    })
+    .delete(function (_, res) {
+        Article.deleteMany()
+            .then(() => res.send("Successfully deleted all articles"))
+            .catch((error) => res.send(error));
     });
-
-    article
-        .save()
-        .then(() => res.send("Successfully added a new article"))
-        .catch((error) => res.send(error));
-});
-
-app.delete("/articles", function (req, res) {
-    Article.deleteMany()
-        .then(() => res.send("Successfully deleted all articles"))
-        .catch((error) => res.send(error));
-});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
